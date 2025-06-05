cc2bb2f90a28ee7ae033ca6ef56fbd3aa921414c
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-06-05 18:26:34 +0600

    Basic Login form and useLoginMutaion

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,40 @@
-
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/api/authApi";
 
 const Login = () => {
-	return (
-		<div>
-			Login page
-		</div>
-	);
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin123456",
+    },
+  });
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data =>", data);
+  console.log("error =>", error);
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+	  password:data.password
+    };
+	login(userInfo)
+  };
+  return (
+    <div>
+      Login page
+      <form onSubmit={handleSubmit(onSubmit)} action="">
+        <div>
+          <label htmlFor="id">Id</label>
+          <input type="text" id="id" {...register("userId")} />
+        </div>
+        <div>
+          <label htmlFor="password">Passwored</label>
+          <input type="text" id="password" {...register("password")} />
+        </div>
+        <Button htmlType="submit">Submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,39 +6,45 @@ import { routeGenerator } from "../utils/routeGenerator";
 import { adminPaths } from "./admin.routes";
 import { studentPaths } from "./student.routes";
 import { facultyPaths } from "./faculty.routes";
-
-
-
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export const router = createBrowserRouter([
-
-	{
-		path:"/",
-		element:<App></App>,
-		children:[
-			{
-				index:true,
-				element:<About></About>,
-			},
-			{
-				path:'contact',
-				element:<Contact></Contact>
-			}
-		]
-	},
-	{
-		path:'/admin',
-		element:<App></App>,
-		children:routeGenerator(adminPaths)
-	},
-	{
-		path:'/student',
-		element:<App></App>,
-		children:routeGenerator(studentPaths)
-	},
-	{
-		path:'/faculty',
-		element:<App></App>,
-		children:routeGenerator(facultyPaths)
-	},
-])
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        index: true,
+        element: <About></About>,
+      },
+      {
+        path: "contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App></App>,
+    children: routeGenerator(adminPaths),
+  },
+  {
+    path: "/student",
+    element: <App></App>,
+    children: routeGenerator(studentPaths),
+  },
+  {
+    path: "/faculty",
+    element: <App></App>,
+    children: routeGenerator(facultyPaths),
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+]);
