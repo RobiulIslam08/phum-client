de1ca85c5fe217839755aecaff3af2d048fae993
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-06-16 12:29:48 +0600

    Setting up get query and current issue with it

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,11 +3,23 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 
+import AcademicSemester from "../pages/admin/academicManagement/AcademicSemester";
+
 export const adminPaths = [
   {
     name: 'Dashboard',
     path: 'dashboard',
     element: <AdminDashboard />,
+  },
+  {
+    name:'Academic Management',
+    children:[
+      {
+        name:'Academic Semester',
+        path:'academic-semester',
+        element:<AcademicSemester/>
+      }
+    ]
   },
   {
     name: 'User Management',
