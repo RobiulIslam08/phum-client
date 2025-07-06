9dc58c7efca6f9fe58b3af58d693f692de78d32f
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-07-06 17:09:08 +0600

    Handle form validation

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -11,13 +11,14 @@ const PHSelect = ({ label, name,options }: TPHSelectProp) => {
   return (
     <Controller
       name={name}
-      render={({ field }) => (
+      render={({ field, fieldState:{error} }) => (
         <Form.Item label={label}>
           <Select
             style={{ width: "100%" }}
             {...field}
             options={options} size="large"
           />
+          {error && <small style={{color:'red'}}>{error.message}</small>}
         </Form.Item>
       )}
     />
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -3,21 +3,12 @@ import PHFrom from "../../../components/form/PHFrom";
 import PHInput from "../../../components/form/PHInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHSelect from "../../../components/form/PHSelect";
+import { semesterOptions } from "../../../constants/semester";
+import { monthOptions } from "../../../constants/global.constant";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
 
-const nameOptions = [
-  {
-    value: "01",
-    label: "Autumn",
-  },
-  {
-    value: "02",
-    label: "Summer",
-  },
-  {
-    value: "03",
-    label: "Fall",
-  },
-];
+
 const currentYear = new Date().getFullYear();
 const yearOptions = [0,1,2,3,4].map((number) => ({
   value:String(currentYear + number),
@@ -26,28 +17,31 @@ const yearOptions = [0,1,2,3,4].map((number) => ({
 }))
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    const name = nameOptions[Number(data.name) - 1]?.label;
+    const name = semesterOptions[Number(data.name) - 1]?.label;
     const semesterData = {
       name,
       code: data.name,
-      year:data.year
+      year:data.year,
+     startMonth :data.startMonth,
+      endMonth:data.endMonth,
+
     };
     console.log(semesterData);
   };
   return (
     <Flex justify="center" align="center">
       <Col span={7}>
-        <PHFrom onSubmit={onSubmit}>
+        <PHFrom onSubmit={onSubmit} resolver={zodResolver(academicSemesterSchema)}>
           {/* <PHInput type="text" name="name" label="Name" />
           <PHInput type="text" name="year" label="Year" /> */}
-          <PHSelect label="Name" name="name" options={nameOptions} />
+          <PHSelect label="Name" name="name" options={semesterOptions} />
           <PHSelect label="Year" name="year" options={yearOptions} />
           <PHSelect
             label="Start Month"
             name="startMonth"
-            options={nameOptions}
+            options={monthOptions}
           />
-          <PHSelect label="End Month" name="endMonth" options={nameOptions} />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
           <Button htmlType="submit">Submit</Button>
         </PHFrom>
       </Col>
